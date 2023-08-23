c6b52bd648f2bbe2fe721e4ca5f9621d3ee285e3
Sean <sean@Seans-MacBook-Pro.local>
2023-08-23 22:24:07 +1000

    added hashmap version

--- 242.ts
+++ 242.ts
@@ -34,3 +34,24 @@ const isAnagram = (s: string, t: string) => {
     return false;
     
 }
+
+const optimalSolution = (s: string, t: string) => {
+    if (s.length != t.length) {
+        return false;
+    }
+    const objA = {};
+    const objB = {};
+    for (let i = 0; i < s.length; i++) {
+        objA[s[i]] ? (objA[s[i]] = 1 + objA[s[i]]) : (objA[s[i]] = 1);
+        objB[t[i]] ? (objB[t[i]] = 1 + objB[t[i]]) : (objB[t[i]] = 1);
+    }
+    console.log(objA);
+    console.log(objB);
+    for (const [key, value] of Object.entries(objA)) {
+        if (objB[key] != value) {
+            console.log(objB[key], value, key);
+            return false;
+        }
+    }
+    return true;
+}
